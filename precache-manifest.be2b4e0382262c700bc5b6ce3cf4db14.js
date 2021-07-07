78729065a74d772e074e6c9f7e331ceae592c2fe
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-07-08 01:15:39 +0530

    Updates

---
+++ precache-manifest.be2b4e0382262c700bc5b6ce3cf4db14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0925a02c968ac2a095cd8091b2387f5c",
+    "url": "/EmojiPedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/EmojiPedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "32c060fca3d1141a7829",
+    "url": "/EmojiPedia/static/js/main.298f722f.chunk.js"
+  },
+  {
+    "revision": "fa389246fbe80faaa303",
+    "url": "/EmojiPedia/static/js/runtime-main.ac8dc35c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/EmojiPedia/precache-manifest.be2b4e0382262c700bc5b6ce3cf4db14.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EmojiPedia/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
